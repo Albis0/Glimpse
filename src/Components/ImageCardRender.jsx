9d444a50f583a19adb204359eb36e3eaf9f1adbe
Abdurrahman Aksakal <abdurrahman.aksakal09@gmail.com>
2026-04-19 04:08:27 +0300

    feat: add skeleton loading with shimmer effect

--- src/Components/ImageCardRender.jsx
+++ src/Components/ImageCardRender.jsx
@@ -1,12 +1,18 @@
 import "../css/ImageCard.css"
 import { FavoriteIcon, } from "./SvgIcons"
 import { FavoritesContext } from "../context/FavoritesContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 function RenderImageCard({ imageUrl, imageApi, onClick, }) {
     const { isFavorite, toggleFavorite } = useContext(FavoritesContext)
+    const [isLoaded, setIsloaded] = useState(false);
     return (
         <div className="imageWrapper" onClick={onClick}>
-            <img src={imageUrl} alt="Image" className="image" />
+            {!isLoaded && <div className="skeletonImage" />}
+            <img
+                src={imageUrl} alt="Image" className="image" loading="lazy"
+                onLoad={() => { setIsloaded(true) }}
+                style={!isLoaded ? { opacity: 0, position: `absolute` } : {}}
+            />
             <span className="iconSpan" onClick={(e) => { e.stopPropagation() }}>
                 <FavoriteIcon size="24" color="azure" filled={isFavorite(imageUrl)} className="favoriteIcon" onClick={() => {
                     toggleFavorite(imageUrl, imageApi)
